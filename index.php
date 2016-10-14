5089a0b29fd1f3fd34cddbf345356662a7ca11d8
imam riyadi <imamriyadi32@gmail.com>
2016-10-15 01:19:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,45 @@
-<?php
-$url = explode('/', $_SERVER['REQUEST_URI']);
-if($url[2]){
-	$nama_function = $url[2]; 
-	$nama_function($url); 
-}else{
-	index();
-}
+<?php 
+require_once('url.php');
 
 function index(){
-	// isi content ...
+	echo "<h1>+ <a href='create'>Daftar </a></h1>";
 }
+
+function create($url){
+	include($url[2].'.php');
+}
+
+function sess_star(){
+	$id = $_POST['id'];
+	$nama = $_POST['nama'];
+	$alamat= $_POST['alamat'];
+	$lv = $_POST['lv'];
+
+	if($id|| $nama|| $alamat || $lv){
+	$data['id_user'] = $id;
+	$data['nama_user'] = $nama;
+	$data['lv_user'] = $lv;
+	$_SESSION['user'] = $data;
+	}else{
+		header("location:daftar");
+	}
+}
+
+function sess_view(){
+	
+	if(isset($_SESSION['user'])){
+		echo $_SESSION['user']['id_user'] . '<br>';
+		echo $_SESSION['user']['nama_user'] . '<br>';
+		echo $_SESSION['user']['lv_user'] . '<br>';
+	}else{
+		echo "Session tidak ada";
+	}
+}
+
+function sess_del(){
+	session_destroy();
+	header("location:index");
+}
+
+
 ?>
